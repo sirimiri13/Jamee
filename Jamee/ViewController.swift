fdbafe4dc202d5a56798817f552092425766d4fb
HuongLam <>
2020-12-25 19:06:10 +0700

    Add Label lauchScreen

---
+++ Jamee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jamee
+//
+//  Created by Huong Lam on 12/25/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
