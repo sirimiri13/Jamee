9ab41dc8d4b620a6373c8bf1108875bda5cfb311
HuongLam <>
2020-12-28 01:46:07 +0700

    refactor voucher view

--- Jamee/Controller/MyVoucherViewController.swift
+++ Jamee/Controller/MyVoucherViewController.swift
@@ -6,6 +6,10 @@
 //
 import Foundation
 import UIKit
+
+
+
+var isHiden: Bool = false
 class Voucher {
     init(name:String,date:String) {
         self.name = name;
@@ -19,28 +23,15 @@ class tableCell: UITableViewCell{
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var CollectButton: UIButton!
+    @IBAction func CollectTapped(_ sender: Any) {
+        CollectButton.isSelected = false
+        CollectButton.tintColor = UIColor.gray
+        CollectButton.setTitle("Collected", for: .normal)
+    }
 }
 
 class MyVoucherViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! tableCell
-        cell.nameLabel.text = listData[indexPath.row].name;
-        cell.dateLabel.text = listData[indexPath.row].date;
-        if (MyVoucherButton.isTouchInside){
-            cell.CollectButton.setTitle("Copy", for: .normal)
-        }
-        else{
-            cell.CollectButton.setTitle("Collect", for: .normal)
-        }
-        cell.CollectButton.setTitleColor(color, for: .normal)
-        return cell
-    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listData.count;
-    }
-    
-    //--- Variable
     let color = UIColor.pinkBackground()
     var botBorder = UIView();
     var listData:Array<Voucher> = [
@@ -48,18 +39,41 @@ class MyVoucherViewController: UIViewController,UITableViewDelegate,UITableViewD
         Voucher(name:"Giam 30% cho hoa don 400k",date:"28/12/2020"),
         Voucher(name:"Giam 15% cho hoa don 500k",date:"25/12/2020"),
     ]
+    var listVoucherGot: Array<Voucher> = []
+    
+    
+    //--- Variable
+   
     //--- Outlet
     @IBOutlet weak var ListVoucherButton: UIButton!
     @IBOutlet weak var MyVoucherButton: UIButton!
     @IBOutlet weak var listVoucherTableView: UITableView!
     
-    @IBAction func CollectAction(_ sender: Any) {
-        let noti = UIAlertController(title: "Noti", message: "This voucher is collected", preferredStyle: .alert)
-        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
-        noti.addAction(okButton)
-        self.present(noti,animated:true,completion:nil)
-    }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //--- List Voucher Button
+        ListVoucherButton.titleLabel?.textColor = color
+        botBorder = UIView(frame: CGRect(x: ListVoucherButton.frame.minX, y: ListVoucherButton.frame.maxY-3, width: ListVoucherButton.frame.width, height: 5));
+        botBorder.backgroundColor = color
+        self.view.addSubview(botBorder)
+        //--- My Voucher Button
+       // MyVoucherButton.layer.borderWidth = 1
+        
+        
+        //--- TableView
+        listVoucherTableView.delegate = self;
+        listVoucherTableView.dataSource = self;
+        // Do any additional setup after loading the view.
+    }
+  
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+        tabBarController?.tabBar.isHidden = true
+    }
     @IBAction func ListVoucherAction(_ sender: Any) {
         botBorder.isHidden = true
         botBorder = UIView(frame: CGRect(x: ListVoucherButton.frame.minX, y: ListVoucherButton.frame.maxY-3, width: ListVoucherButton.frame.width, height: 5));
@@ -67,6 +81,7 @@ class MyVoucherViewController: UIViewController,UITableViewDelegate,UITableViewD
         ListVoucherButton.setTitleColor(color, for: .normal)
         MyVoucherButton.setTitleColor(.black, for: .normal)
         self.view.addSubview(botBorder)
+        isHiden = false
         listVoucherTableView.reloadData()
     }
     @IBAction func MyVoucherAction(_ sender: Any) {
@@ -77,54 +92,29 @@ class MyVoucherViewController: UIViewController,UITableViewDelegate,UITableViewD
         MyVoucherButton.setTitleColor(color, for: .normal)
         self.view.addSubview(botBorder)
         listVoucherTableView.reloadData()
+        isHiden = true;
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //--- List Voucher Button
-        ListVoucherButton.layer.borderWidth = 1
-        ListVoucherButton.titleLabel?.textColor = color
-        botBorder = UIView(frame: CGRect(x: ListVoucherButton.frame.minX, y: ListVoucherButton.frame.maxY-3, width: ListVoucherButton.frame.width, height: 5));
-        botBorder.backgroundColor = color
-        self.view.addSubview(botBorder)
-        //--- My Voucher Button
-        MyVoucherButton.layer.borderWidth = 1
-        
-        
-        //--- TableView
-        listVoucherTableView.delegate = self;
-        listVoucherTableView.dataSource = self;
-        // Do any additional setup after loading the view.
-    }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath) as! tableCell
+        cell.nameLabel.text = listData[indexPath.row].name;
+        cell.dateLabel.text = listData[indexPath.row].date;
+        if (isHiden == true){
+            cell.CollectButton.isHidden = true
+        }
+        else {
+            cell.CollectButton.isHidden = false
+        }
+        
+        cell.CollectButton.tintColor = UIColor.pinkBackground()
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listData.count;
+    }
+    
 }
 
-
-extension UIColor {
-   convenience init(red: Int, green: Int, blue: Int) {
-       assert(red >= 0 && red <= 255, "Invalid red component")
-       assert(green >= 0 && green <= 255, "Invalid green component")
-       assert(blue >= 0 && blue <= 255, "Invalid blue component")
-
-       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-   }
-
-   convenience init(rgb: Int) {
-       self.init(
-           red: (rgb >> 16) & 0xFF,
-           green: (rgb >> 8) & 0xFF,
-           blue: rgb & 0xFF
-       )
-   }
-}
